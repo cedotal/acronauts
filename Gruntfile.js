cbb4471d9c36b6e9a74d893c7d5c8f1a4fb4a59e
Andrew Cedotal <andrew.cedotal@gmail.com>
2013-12-12 08:45:18 -0500

    converted Makefile to Gruntfile; added grunt tasks for running mocha tests and jshinting code

---
+++ Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        jshint: {
+            src: ['*.js', 'public/client.js', 'test/*.js'],
+            options: {
+                curly: true,
+                eqeqeq: true,
+                immed: true,
+                latedef: true,
+                newcap: true,
+                noarg: true,
+                sub: true,
+                undef: true,
+                boss: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    require: true,
+                    define: true,
+                    requirejs: true,
+                    describe: true,
+                    expect: true,
+                    it: true
+                }
+            }
+        },
+        watch: {
+            files: '<%= jshint.src %>',
+            tasks: ['jshint', 'mochaTest']
+        },
+        mochaTest: {
+            test: {
+                src: ['test/*.js']
+            }
+        }
+    });
+
+    // Load tasks
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+
+    // Default task.
+    grunt.registerTask('default', 'jshint');
+    grunt.registerTask('test', 'mochaTest');
+};
