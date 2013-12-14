e4c4a3139bc6ed115bb59e0574c8dadda6d7b8ae
Andrew Cedotal <andrew.cedotal@gmail.com>
2013-12-14 13:43:12 -0500

    added contextual jshint options to each jshint subtask (node: true, browser: true, etc.)

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,16 +17,42 @@ module.exports = function(grunt) {
             // TODO: add specific options for each sub-task relfecting the expectations for each
             // environment (i.e., node: true)
             client: {
-                src: './public/client.js'
+                src: './public/client.js',
+                options: {
+                    browser: true,
+                    jquery: true,
+                    devel: true,
+                    globals: {
+                        // socket.io object
+                        io: true
+                    }
+                }
             },
             server: {
-                src: './server/*'
+                src: './server/*',
+                options: {
+                    devel: true,
+                    node: true
+                }
             },
             test: {
-                src: './test/*.js'
+                src: './test/*.js',
+                options: {
+                    expr: true,
+                    node: true,
+                    globals: {
+                        // should.js objects
+                        it: true,
+                        should: true,
+                        describe: true
+                    }
+                }
             },
             tools: {
-                src: ['./*.js', './*.json', './.jshintrc']
+                src: ['./*.js', './*.json', './.jshintrc'],
+                options: {
+                    node: true
+                }
             }
         },
         watch: {
