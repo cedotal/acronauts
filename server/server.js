8c110d283539f31c080f8c8ae2cdcfc29acc00b5
Andrew Cedotal <andrew.cedotal@gmail.com>
2013-12-16 21:58:07 -0500

    removed stray refs to models in server.js

--- server/server.js
+++ server/server.js
@@ -14,20 +14,6 @@ app.get('/', function(req, res){
 // set up static file server
 app.use(express.static(__dirname + '/../public'));
 
-// set up game config
-var gameConfig = {
-	promptLength: 5, // the number of characters in an acronauts prompt
-	clockLength: 60, // the amount of time players have to answer the prompt
-	gameStartDelay: 15, // the minimum amount of time players should have
-	idealGameWait: 5, // the maximum amount of time that players will ideally wait for additional players to be added on top of minPlayers 
-	maxPlayers: 8, // the most players we should have in a game
-	minPlayers: 3, // the fewest players we should have in a game
-	ignoredCharacters: ['\'', '\"'], // characters that will always be ignored if part of an answer
-	optionallyIgnoredWords: ['a', 'an', 'the'] // words that can be either counted or not counted as part of an answer
-};
-
-var Player = require('./models/Player');
-var Game = require('./models/Game');
 var Lobby = require('./models/Lobby');
 
 // set up socket.io listener and events
