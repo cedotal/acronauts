ddf4ad4ab340266e89d922afce6fc3cb4ae698de
Andrew Cedotal <andrew.cedotal@gmail.com>
2013-12-20 21:05:05 -0500

    dummy insertion of env config options into client by server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,7 @@
 // set up express
 
+console.log(process.env.NODE_ENV);
+
 var express = require('express');
 var app = express();
 
@@ -9,6 +11,18 @@ app.set('view engine', 'jade');
 app.engine('jade', require('jade').__express);
 app.get('/', function(req, res){
 	res.render('page');
+});
+
+// special rule so that we can have the client recieve different config files
+// based on the environment
+// TODO: make this actually send dynamic data
+app.use(function(req, res, next){
+    if (req.url === '/js/config.js'){
+        var response = 'define([], function(){ return { "host": "testhost" }});';
+        res.send(response);
+    } else {
+        next();
+    }
 });
 
 // set up static file server
